575add8478b93c870d630bcb6710e5a4000f801b
kyuhwanyeon <kyuhwanyeon@gmail.com>
2021-02-01 00:53:07 +0900

    Modify map and add the comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,58 +10,50 @@
 using std::cout;
 using std::endl;
 using namespace std;
-vector<vector<int>> GRID = {
-  {0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,},
-  {0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,},
-  {0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,},
-  {0,1,1,0,0,0,0,1,1,0,0,0,1,1,1,0,},
-  {0,1,1,0,0,0,1,1,0,0,0,1,1,1,0,0,},
-  {0,1,1,0,0,1,1,0,0,0,1,1,1,0,0,0,},
-  {0,1,1,0,1,1,0,0,0,0,1,1,0,0,0,0,},
-  {0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,},
-  {0,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,},
-  {0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,},
-  {0,1,0,0,0,1,1,1,0,0,1,1,1,1,1,1,},
-  {0,0,0,0,1,1,1,0,0,1,1,1,1,1,1,1,},
-  {0,0,0,1,1,1,0,0,1,1,1,1,1,1,1,1,},
-  {0,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,},
-  {0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,},
-  {1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,},};
-  // {1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,}};
+vector<vector<int>> grid = {
+  {0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,},
+  {0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,},
+  {0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,},
+  {0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,0,},
+  {0,0,1,0,0,0,1,0,0,0,0,1,1,1,0,0,},
+  {0,0,1,0,0,1,0,0,0,0,1,1,1,0,0,0,},
+  {0,0,1,0,1,0,0,0,0,0,1,1,0,0,0,0,},
+  {0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,},
+  {0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,},
+  {0,0,0,0,1,0,1,0,0,0,1,1,1,1,1,1,},
+  {0,0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,},
+  {0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,},
+  {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},};
 
-vector<double> start_pos = {0.0f, 0.0f, 0.0f};
-vector<double> goal_pos = {(double)GRID.size()-1, (double)GRID[0].size()-1, 0.0f};
+vector<double> start_pos = {0.5f, 0.5f, 0.0f};
+vector<double> goal_pos = {15.5, 15.5, M_PI_2};
 
 int main() {
-  cout << "Finding path through grid:" << endl;
-  
-  // Creates an Empty Maze and for testing the number of expansions with it
-  for(int i = 0; i < GRID.size(); ++i) {
-    cout << GRID[i][0];
-    for(int j = 1; j < GRID[0].size(); ++j) {
-      cout << "," << GRID[i][j];
-    }
-    cout << endl;
-  }
-  printf("x: %d, y: %d \n", GRID.size(),GRID[0].size());
-
-  HybridAStar hybrid_a_star = HybridAStar(start_pos, goal_pos, GRID);
-  Viewer viewer = Viewer(GRID);
+  HybridAStar hybrid_a_star = HybridAStar(start_pos, goal_pos, grid);
+  // viewer construct with grid
+  Viewer viewer = Viewer(grid);
+  // Hybrid a start search
   Path get_path = hybrid_a_star.Search();
-
-  vector<State> show_path = hybrid_a_star.ReconstructPath(get_path.came_from, 
-                                                       start_pos, get_path.final);
-
-  cout << "show path from start to finish" << endl;
-  for(int i = show_path.size()-1; i >= 0; --i) {
-      State step = show_path[i];
-      cout << "##### step " << step.g << " #####" << endl;
-      cout << "x " << step.x << endl;
-      cout << "y " << step.y << endl;
-      cout << "theta " << step.theta << endl;
-  }
-  
-  viewer.set_visited_path(show_path);
+  // Reconstruct the path for viewer
+  vector<State> show_path = hybrid_a_star.ReconstructPath(get_path.came_from, get_path.final);
+  // Get open lists for viewer
+  vector<vector<State>> opened_lists = hybrid_a_star.get_opened_visulizer();
+  // Visualize the open lists
   viewer.show_image();
+  waitKey( 0);
+  for (int i = 0; i < opened_lists.size(); i++)
+  {
+    viewer.set_visited_path(opened_lists[i]);
+    viewer.show_image();
+    viewer.clear_image();
+  }
+  // Visualize final path
+  viewer.set_visited_path(show_path);
+  viewer.poly_path();
+  viewer.show_image();
+  waitKey( 0 );
   return 0;
 }
