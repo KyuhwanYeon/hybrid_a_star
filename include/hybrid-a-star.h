d57b72ead134b89e73cb9ed6fd777d4271e30d0b
kyuhwanyeon <kyuhwanyeon@gmail.com>
2021-01-31 23:52:13 +0900

    Add collision check

--- include/hybrid-a-star.h
+++ include/hybrid-a-star.h
@@ -3,7 +3,13 @@
 #include "struct.h"
 #include <vector>
 using std::vector;
-
+#define MAX_STEERING 90
+#define DEG2RADIAN M_PI/180.0
+#define RADIAN2DEG 57.2958
+#define DT 1
+#define SPEED 1.5
+#define NUM_THETA_CELLS 180
+#define THRESHOLD_GOAL_THETA 10
 class HybridAStar
 {
 public:
@@ -25,17 +31,21 @@ public:
                                 vector<double> &start, State &final);
 
   Path Search();
-  double Heuristic(double x1, double y1, double x2, double y2);
+
+  vector<vector<State>> get_opened_visulizer(void);
+private:
+  double Heuristic(double x1, double y1, double theta1, double x2, double y2, double theta2);
   void Sort(vector<State> *v);
   bool static Compare(State v1, State v2);
-private:
-  const int NUM_THETA_CELLS = 90;
-  const double SPEED = 1.45;
-  const double LENGTH = 0.5;
+  bool is_collision(const double& x, const double& y, const double& theta);
+  bool is_out_of_map(const double& x, const double& y);
+  bool is_grid_collision(const double& x, const double& y);
+
   vector<double> start_pos_;
   vector<double> goal_pos_;
   vector<vector<int>> grid_;
   vector<int> goal_idx_;
+  vector<vector<State>> opend_lists_visualizer_;
 };
 
 #endif // HYBRID_A_STAR_H
--- src/hybrid-a-star.cpp
+++ src/hybrid-a-star.cpp
@@ -47,9 +47,11 @@ void HybridAStar::Sort(vector<State> *v) {
   sort(v->begin(), v->end(), HybridAStar::Compare);
 }
 
-double HybridAStar::Heuristic(double x1, double y1, double x2, double y2)
+double HybridAStar::Heuristic(double x1, double y1, double theta1, double x2, double y2, double theta2)
 {
-    return abs(x1-x2)+abs(y1-y2);
+  double euclidian_distance = sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+  double angle_err = abs(theta1 - theta2);
+  return euclidian_distance+angle_err;
 
 }
 
@@ -63,23 +65,24 @@ vector<State> HybridAStar::Expand(State &state) {
   int next_g = g+1;
   vector<State> next_states;
 
-  for(double delta_i = -40; delta_i < 45; delta_i+=5) {
-    double delta = M_PI / 180.0 * delta_i;
-    double omega = SPEED / LENGTH * tan(delta);
+  for(double delta_i = -MAX_STEERING; delta_i <= MAX_STEERING; delta_i+=5) {
+    double delta = DEG2RADIAN * delta_i;
+    double omega = SPEED / VEHICLE_LEGTH * (delta) * DT;
     double next_theta = theta + omega;
     if(next_theta < 0) {
       next_theta += 2*M_PI;
     }
 		// bicycle model
-    double next_x = x + SPEED * cos(theta);
-    double next_y = y + SPEED * sin(theta);
+    double next_x = x + SPEED * cos(theta)* DT;
+    double next_y = y + SPEED * sin(theta)* DT;
     State next_state;
     next_state.g = next_g;
     next_state.x = next_x;
     next_state.y = next_y;
-    next_state.h = Heuristic(next_x, next_y, goal_pos_[0], goal_pos_[1]);
-    next_state.f = next_state.g + next_state.h;
+
     next_state.theta = next_theta;
+    next_state.h = Heuristic(next_x, next_y, next_state.theta, goal_pos_[0], goal_pos_[1], goal_pos_[2]);
+    next_state.f = next_state.g + next_state.h;
     next_states.push_back(next_state);
   }
 
@@ -127,7 +130,7 @@ Path HybridAStar::Search() {
 
   State state;
   state.g = g;
-  state.h = Heuristic(start_pos_[0], start_pos_[1], goal_pos_[0], goal_pos_[1]);
+  state.h = Heuristic(start_pos_[0], start_pos_[1], start_pos_[2], goal_pos_[0], goal_pos_[1], goal_pos_[2]);
   state.f = state.g + state.h;
   state.x = start_pos_[0];
   state.y = start_pos_[1];
@@ -142,10 +145,11 @@ Path HybridAStar::Search() {
     Sort(&opened); // opened is sorted by f
     State current = opened[0]; 
     opened.erase(opened.begin()); // pop current state
-
+    opend_lists_visualizer_.push_back(opened); // just for visualizer
     int x = current.x;
     int y = current.y;
-    if(Idx(x) == goal_idx_[0] && Idx(y) == goal_idx_[1]) {
+    double theta = current.theta;
+    if(Idx(x) == goal_idx_[0] && Idx(y) == goal_idx_[1] && (RADIAN2DEG*fabs(theta-goal_pos_[2])<THRESHOLD_GOAL_THETA)) {
       std::cout << "found path to goal in " << total_closed << " expansions" 
                 << std::endl;
       Path path;
@@ -164,14 +168,12 @@ Path HybridAStar::Search() {
       double y2 = next_state[i].y;
       double theta2 = next_state[i].theta;
 
-      if((x2 < 0 || x2 >= grid_.size()) || (y2 < 0 || y2 >= grid_[0].size())) {
-        // invalid cell
+      if (is_collision(x2, y2, theta2))
         continue;
-      }
 
       int stack2 = Theta2Stack(theta2);
 
-      if(closed[stack2][Idx(x2)][Idx(y2)] == 0 && grid_[Idx(x2)][Idx(y2)] == 0) {
+      if(closed[stack2][Idx(x2)][Idx(y2)] == 0) {
         opened.push_back(next_state[i]);
         closed[stack2][Idx(x2)][Idx(y2)] = 1;
         came_from[stack2][Idx(x2)][Idx(y2)] = current;
@@ -187,4 +189,64 @@ Path HybridAStar::Search() {
   path.final = state;
 
   return path;
+}
+
+vector<vector<State>> HybridAStar::get_opened_visulizer(void)
+{
+  return opend_lists_visualizer_;
+}
+
+bool HybridAStar::is_collision(const double& x, const double& y, const double& theta)
+{
+  double margin = 0.3;
+  bool ret = false; 
+  double rear_center_x = x;
+  double rear_center_y = y;
+  double front_center_x = rear_center_x + (VEHICLE_LEGTH+margin)*cos(theta);
+  double front_center_y = rear_center_y + (VEHICLE_LEGTH+margin)*sin(theta);
+  double left_rear_x = rear_center_x + (VEHICLE_WIDTH+margin)/2*cos(theta + M_PI/2);
+  double left_rear_y = rear_center_y + (VEHICLE_WIDTH+margin)/2*sin(theta + M_PI/2);
+  double right_rear_x = rear_center_x + (VEHICLE_WIDTH+margin)/2*cos(M_PI/2 - theta);
+  double right_rear_y = rear_center_y - (VEHICLE_WIDTH+margin)/2*sin(M_PI/2 - theta);
+
+  double left_front_x = front_center_x + (VEHICLE_WIDTH+margin)/2*cos(theta + M_PI/2);
+  double left_front_y = front_center_y + (VEHICLE_WIDTH+margin)/2*sin(theta + M_PI/2);
+  double right_front_x = front_center_x + (VEHICLE_WIDTH+margin)/2*cos(M_PI/2 - theta);
+  double right_front_y = front_center_y - (VEHICLE_WIDTH+margin)/2*sin(M_PI/2 - theta);
+  vector<vector<double>> vertices;
+  vertices.push_back({left_rear_x, left_rear_y});
+  vertices.push_back({right_rear_x, right_rear_y});
+  vertices.push_back({left_front_x, left_front_y});
+  vertices.push_back({right_front_x, right_front_y});
+  for (int i = 0 ; i < vertices.size(); i ++)
+  {
+    bool out_map = is_out_of_map(vertices[i][0], vertices[i][1]);
+    if (out_map == true)
+    {
+      ret = true;
+      break;
+    }
+    bool collision_grid = is_grid_collision(vertices[i][0], vertices[i][1]);
+    if (collision_grid == true)
+    {
+      ret = true;
+      break;
+    }
+  }
+
+  return ret;
+}
+bool HybridAStar::is_out_of_map(const double& x, const double& y)
+{
+  if((x < 0 || x >= grid_.size()) || (y < 0 || y >= grid_[0].size()))
+    return true;
+  else
+    return false;  
+}
+bool HybridAStar::is_grid_collision(const double& x, const double& y)
+{
+  if (grid_[Idx(x)][Idx(y)] == 1)
+    return true;
+  else
+    return false;
 }
